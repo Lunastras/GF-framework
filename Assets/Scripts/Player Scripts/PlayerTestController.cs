47f3a1d12eb0e40d0fa9e3dda253cf9190d2aa5f
Lunastras <radu_bucurescu@yahoo.com>
2022-10-21 12:51:07 +0100

    Revert "new update"
    This reverts commit d6c9f76f141e3b2402c5db1749459448f09f145e.

--- Assets/Scripts/Player Scripts/PlayerTestController.cs
+++ Assets/Scripts/Player Scripts/PlayerTestController.cs
@@ -1,86 +1,122 @@
-en looking along the y-axis towards the origin in radians.</param>
-        /// <returns>The RigidTransform of rotating around the y-axis by the given angle.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static RigidTransform RotateY(float angle)
-        {
-            return new RigidTransform(quaternion.RotateY(angle), float3.zero);
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using static System.MathF;
+
+public class PlayerTestController : MovementGeneric
+{
+    [SerializeField]
+    private float acceleration = 10;
+    [SerializeField]
+    private float deacceleration = 10;
+    [SerializeField]
+    private float midAirAcceleration = 10;
+    [SerializeField]
+    private float midAirDeacceleration = 10;
+    [SerializeField]
+    private float maxFallSpeed = 40;
+    [SerializeField]
+    private float jumpForce = 40;
+    public float AccelerationCoef {get; set;} = 1;
+    public float DeaccelerationCoef {get; set;} = 1;
+    private float _effectiveDeacceleration;
+    private float _effectiveAcceleration;
+    public bool CanDoubleJump { get; protected set; }
+    public bool CanExtendJump { get; protected set; }
+
+    //whether we touched the current parent this frame or not
+    private bool touchedParent;
+
+    // Start is called before the first frame update
+    protected override void InternalStart() { }
+
+    public override void CalculateMovement(float speedMultiplier = 1) { }
+
+    protected override void BeforeFixedUpdate()
+    {
+        touchedParent = false;
+        //Debug.Log("CALC MOVEMENT WAS CAALLED, GROUNDED IS " + IsGrounded);
+        if (SlopeNormal != Vector3.up) {
+            Quaternion q = GfTools.RotationTo(Vector3.up, SlopeNormal);
+            MovementDir = q * MovementDir;
+        }
+        CalculateEffectiveValues();
+        CalculateVelocity();
+        CalculateJump();
+    }
+
+    protected override void AfterFixedUpdate() {
+        if(!touchedParent && null != parentTransform) 
+            DetachFromParent();
+    }
+    
+    protected void CalculateEffectiveValues() {
+        if(IsGrounded) {
+            _effectiveAcceleration = acceleration;
+            _effectiveDeacceleration = deacceleration;
+        } else {
+            _effectiveAcceleration = midAirAcceleration;
+            _effectiveDeacceleration = midAirDeacceleration;
+        }
+        _effectiveAcceleration *= AccelerationCoef;
+        _effectiveDeacceleration *= DeaccelerationCoef;
+    }
+
+    void CalculateVelocity()
+    {
+        float verticalFallSpeed = (Velocity.magnitude * Vector3.Dot(SlopeNormal, Velocity.normalized));
+        float fallMagn, fallMaxDiff = -verticalFallSpeed - maxFallSpeed;
+
+        if(fallMaxDiff < 0) { //speed under maxFallSpeed
+            fallMagn = Min(-fallMaxDiff, mass * Time.deltaTime);
+        } else { //speed equal to maxFallSpeed or higher
+            fallMagn = -Min(fallMaxDiff, _effectiveDeacceleration * Time.deltaTime);
         }
 
-        /// <summary>Returns a RigidTransform that rotates around the z-axis by a given number of radians.</summary>
-        /// <param name="angle">The clockwise rotation angle when looking along the z-axis towards the origin in radians.</param>
-        /// <returns>The RigidTransform of rotating around the z-axis by the given angle.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static RigidTransform RotateZ(float angle)
-        {
-            return new RigidTransform(quaternion.RotateZ(angle), float3.zero);
-        }
+        //remove vertical factor from the velocity to calculate the horizontal plane velocity easier
+        Vector3 horizontalVelocity = Velocity - SlopeNormal * verticalFallSpeed;
 
-        /// <summary>Returns a RigidTransform that translates by an amount specified by a float3 vector.</summary>
-        /// <param name="vector">The translation vector.</param>
-        /// <returns>The RigidTransform that translates by the given translation vector.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static RigidTransform Translate(float3 vector)
-        {
-            return new RigidTransform(quaternion.identity, vector);
-        }
+        float currentSpeed = horizontalVelocity.magnitude;
+        float dotMovementVelDir = Vector3.Dot(MovementDir, currentSpeed > 0.00001F ? horizontalVelocity / currentSpeed : Vector3.zero);
 
+        float desiredSpeed = speed * movementDirMagnitude;
+        float speedInDesiredDir = currentSpeed * Max(0, dotMovementVelDir);
 
-        /// <summary>Returns true if the RigidTransform is equal to a given RigidTransform, false otherwise.</summary>
-        /// <param name="x">The RigidTransform to compare with.</param>
-        /// <returns>True if the RigidTransform is equal to the input, false otherwise.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool Equals(RigidTransform x) { return rot.Equals(x.rot) && pos.Equals(x.pos); }
+        Vector3 unwantedVelocity = horizontalVelocity - MovementDir * Min(speedInDesiredDir, desiredSpeed);    
+        float unwantedSpeed = unwantedVelocity.magnitude;
+        unwantedVelocity = unwantedSpeed > 0.00001F ? unwantedVelocity / unwantedSpeed : Vector3.zero; //normalised
 
-        /// <summary>Returns true if the RigidTransform is equal to a given RigidTransform, false otherwise.</summary>
-        /// <param name="x">The object to compare with.</param>
-        /// <returns>True if the RigidTransform is equal to the input, false otherwise.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public override bool Equals(object x) { return x is RigidTransform converted && Equals(converted); }
+        Velocity +=   MovementDir * Min(Max(0, desiredSpeed - speedInDesiredDir), Time.deltaTime * _effectiveAcceleration) //acceleration force
+                    - unwantedVelocity * Min(unwantedSpeed, _effectiveDeacceleration * Time.deltaTime)                     //deacceleration force
+                    - SlopeNormal * fallMagn;                                                                              //fall force
+    }
 
-        /// <summary>Returns a hash code for the RigidTransform.</summary>
-        /// <returns>The hash code of the RigidTransform.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public override int GetHashCode() { return (int)math.hash(this); }
-
-        /// <summary>Returns a string representation of the RigidTransform.</summary>
-        /// <returns>The string representation of the RigidTransform.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public override string ToString()
-        {
-            return string.Format("RigidTransform(({0}f, {1}f, {2}f, {3}f),  ({4}f, {5}f, {6}f))",
-                rot.value.x, rot.value.y, rot.value.z, rot.value.w, pos.x, pos.y, pos.z);
-        }
-
-        /// <summary>Returns a string representation of the RigidTransform using a specified format and culture-specific format information.</summary>
-        /// <param name="format">The format string.</param>
-        /// <param name="formatProvider">The format provider to use during formatting.</param>
-        /// <returns>The formatted string representation of the RigidTransform.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public string ToString(string format, IFormatProvider formatProvider)
-        {
-            return string.Format("float4x4(({0}f, {1}f, {2}f, {3}f),  ({4}f, {5}f, {6}f))",
-                rot.value.x.ToString(format, formatProvider),
-                rot.value.y.ToString(format, formatProvider),
-                rot.value.z.ToString(format, formatProvider),
-                rot.value.w.ToString(format, formatProvider),
-                pos.x.ToString(format, formatProvider),
-                pos.y.ToString(format, formatProvider),
-                pos.z.ToString(format, formatProvider));
+    void CalculateJump() 
+    { 
+        if(JumpTrigger) {
+            JumpTrigger = false;
+            if(jumpTriggerReleased) {
+                jumpTriggerReleased = false;
+                Velocity.y = jumpForce;
+            }
+        } else {
+            jumpTriggerReleased = true;
         }
     }
 
-    public static partial class math
+    protected override void MgOnCollision(MgCollisionStruct collision)
     {
-        /// <summary>Returns a RigidTransform constructed from a rotation represented by a unit quaternion and a translation represented by a float3 vector.</summary>
-        /// <param name="rot">The quaternion rotation.</param>
-        /// <param name="pos">The translation vector.</param>
-        /// <returns>The RigidTransform of the given rotation quaternion and translation vector.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static RigidTransform RigidTransform(quaternion rot, float3 pos) { return new RigidTransform(rot, pos); }
+        Debug.Log("I came into collision WITH " + collision.collider.name);
+        Transform collisionTrans = collision.collider.transform;
+        bool auxGrounded = slopeLimit >= collision.angle;
 
-        /// <summary>Returns a RigidTransform constructed from a rotation represented by a float3x3 rotation matrix and a translation represented by a float3 vector.</summary>
-        /// <param name="rotation">The float3x3 rotation matrix.</param>
-        /// <param name="translation">The translation vector.</param>
-        /// <returns>The RigidTransform of the given rotation matrix and translation vector.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static RigidTransform RigidTransform(float3x3 rotation, float3 translat
+        if(auxGrounded && parentTransform == null) {
+            Debug.Log("I am supposed to parent something");
+            SetParentTransform(collisionTrans);
+        }
+
+        touchedParent |= auxGrounded && parentTransform == collisionTrans;
+    }
+
+}
